992d8905e0c457195c817f072f2ed410455e2068
Marco Ciucci <marco.ciucci@proton.me>
2024-08-05 22:17:14 +0200

    restructure, added include

---
+++ include/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+    $config['skin'] = "jobhunt";
+    # $config['dashboard'] = "admin";
+
+    $config['dbms']['localhost']['user'] = "admin";
+    $config['dbms']['localhost']['passwd'] = "admin";
+    $config['dbms']['localhost']['host'] = "localhost";
+    $config['dbms']['localhost']['dbname'] = "ccc";
+
+    # $config['dbms']['193.204.135.234']['user'] = "u_28762187";
+    # $config['dbms']['193.204.135.234']['passwd'] = "u_28762187";
+    # $config['dbms']['193.204.135.234']['host'] = "193.204.135.234";
+    # $config['dbms']['193.204.135.234']['dbname'] = "u_28762187";
+
+?>
