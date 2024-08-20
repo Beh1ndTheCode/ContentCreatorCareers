76aeb8f4b631f1208477bf106dcbeb00894b9c2d
StefanoElisio <stfnelisio.work@gmail.com>
2024-08-20 17:54:10 +0200

    added candidates php pages

---
+++ candidates_my_resume_add_new.php
@@ -0,0 +1,19 @@
+<?php
+
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    require "include/config.inc.php";
+    require "include/dbms.inc.php";
+    require "include/template2.inc.php";
+    require "include/auth.inc.php";
+
+    $main = new Template("frame");
+    $body = new Template("candidates_my_resume_add_new");
+
+    $main->setContent("body", $body->get());
+
+    $main->close();
+
+?>
