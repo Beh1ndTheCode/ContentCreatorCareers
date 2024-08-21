b4540e9a68558661080338d4ab651671cc0c9296
StefanoElisio <stfnelisio.work@gmail.com>
2024-08-21 16:11:25 +0200

    added reagistration.php

---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php
+
+require "include/config.inc.php";
+require "include/dbms.inc.php";
+
+//query per aggiungerel'user al database, con in input: username, password, email, e tipo di utente (candidate o employer)
