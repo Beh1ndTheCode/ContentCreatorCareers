56837378ecb33b4aa87720d1e0198b261595170a
Marco Ciucci <marco.ciucci@proton.me>
2024-08-21 18:21:10 +0200

    index templating

--- index.php
+++ index.php
@@ -12,6 +12,41 @@ $main = new Template("frame");
 
 $body = new Template("index");
 
+$result = $mysqli->query("SELECT COUNT(*) AS jobs_count FROM job_offer");
+
+$data = $result->fetch_assoc();
+$body->setContent("jobs_count", $data['jobs_count']);
+
+$result = $mysqli->query("
+	SELECT title, COUNT(*) AS jobs_count_category FROM expertise
+	JOIN profile_expertise ON profile_expertise.expertise_id = expertise.id
+	JOIN profile ON profile_expertise.profile_id = profile.id
+	JOIN employer ON profile.id = employer.id
+	JOIN job_offer ON employer.id = job_offer.employer_id
+	");
+
+$data = $result->fetch_assoc();
+
+$body->setContent("expertise", $data['title']);
+$body->setContent("jobs_count_category", $data['jobs_count_category']);
+
+$result = $mysqli->query("
+	SELECT
+	    job_offer.name AS job_name, 
+		employer.name AS employer_name, 
+		address.city AS city, 
+		address.country AS country 
+	FROM job_offer
+	JOIN employer ON job_offer.employer_id = employer.id
+	JOIN address ON employer.id = address.profile_id
+	");
+
+$data = $result->fetch_assoc();
+$body->setContent("job_name", $data['job_name']);
+$body->setContent("employer_name", $data['employer_name']);
+$body->setContent("city", $data['city']);
+$body->setContent("country", $data['country']);
+
 $main->setContent("body", $body->get());
 
 $main->close();
