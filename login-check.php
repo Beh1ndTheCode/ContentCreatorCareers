5c5accc9707738f2efa0fc7de2e81dbe5a21b6b9
Marco Ciucci <marco.ciucci@proton.me>
2024-08-22 20:14:11 +0200

    changed to prepared statements

--- login-check.php
+++ login-check.php
@@ -4,19 +4,28 @@ require "include/config.inc.php";
 require "include/dbms.inc.php";
 
 # $passwd = md5("{$_POST['password']}");
-
 # $result = $mysqli->query("SELECT username, email FROM `user` WHERE username = '{$_POST['username']}' AND password = '{$passwd}'");
 
-$result = $mysqli->query("SELECT username, email FROM `user` WHERE username = '{$_POST['username']}' AND password = '{$_POST['password']}'");
+$stmt = $mysqli->prepare("SELECT username, email FROM `user` WHERE username = ? AND password = ?");
 
+// Bind the parameters to the placeholders
+$stmt->bind_param("ss", $_POST['username'], $_POST['password']);
 
+// Execute the statement
+$stmt->execute();
+
+// Get the result
+$result = $stmt->get_result();
+
+// Check if the user exists
 if ($result->num_rows == 1) {
-    $result = 1;
+    $result = 1; // User exists
 } else {
-    $result = 0;
+    $result = 0; // User does not exist
 }
-
 
 echo json_encode($result);
 
-
+// Close the statement and connection
+$stmt->close();
+$mysqli->close();
--- signup_check.php
+++ signup_check.php
@@ -3,19 +3,36 @@
 require "include/config.inc.php";
 require "include/dbms.inc.php";
 
-# $passwd = md5("{$_POST['password']}");
+// Prepare the statements to avoid SQL injection
+$stmt_username = $mysqli->prepare("SELECT 'username' FROM `user` WHERE 'username' = ?");
+$stmt_email = $mysqli->prepare("SELECT 'email' FROM `user` WHERE 'email' = ?");
 
-# $result = $mysqli->query("SELECT username, email FROM `user` WHERE username = '{$_POST['username']}' AND password = '{$passwd}'");
+// Bind parameters and execute for username
+$stmt_username->bind_param("s", $_POST['username']);
+$stmt_username->execute();
+$stmt_username->store_result();
+$username_exists = $stmt_username->num_rows > 0;
 
-$result_username = $mysqli->query("SELECT username FROM `user` WHERE username = '{$_POST['username']}'");
-$result_email = $mysqli->query("SELECT email FROM `user` WHERE email = '{$_POST['email']}'");
+// Bind parameters and execute for email
+$stmt_email->bind_param("s", $_POST['email']);
+$stmt_email->execute();
+$stmt_email->store_result();
+$email_exists = $stmt_email->num_rows > 0;
 
-if ($result_username->num_rows != 0) {
+// Determine the result
+if ($username_exists && $email_exists) {
     $result = 0;
-} elseif($result_email->num_rows != 0) {
+} elseif ($username_exists) {
     $result = 1;
-} else{
+} elseif ($email_exists) {
     $result = 2;
+} else {
+    $result = 3;
 }
 
+
 echo json_encode($result);
+
+$stmt_username->close();
+$stmt_email->close();
+$mysqli->close();
