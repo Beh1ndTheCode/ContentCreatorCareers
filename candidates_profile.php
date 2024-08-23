21d206446edc12d177827a21f62867dc0406ca9e
StefanoElisio <stfnelisio.work@gmail.com>
2024-08-23 15:13:16 +0200

    candidates profile templating

--- candidates_profile.php
+++ candidates_profile.php
@@ -10,6 +10,20 @@ require "include/auth.inc.php";
 $main = new Template("frame");
 $body = new Template("candidates_profile");
 
+$result = $mysqli->query("
+    SELECT
+        candidate.name AS name,
+        candidate.surname AS surname,
+        candidate.age AS age
+    FROM candidate
+    ");
+
+$data = $result->fetch_assoc();
+$body->setContent("name", $data['name']);
+$body->setContent("surname", $data['surname']);
+$body->setContent("age", $data['age']);
+
+
 $main->setContent("body", $body->get());
 
 $main->close();
