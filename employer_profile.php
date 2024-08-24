539097f5ae928239723beb694dba227f432c5957
Marco Ciucci <marco.ciucci@proton.me>
2024-08-24 18:31:33 +0200

    prepared statements for registration

--- employer_profile.php
+++ employer_profile.php
@@ -1,19 +1,15 @@
 <?php
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+session_start();
 
-    require "include/config.inc.php";
-    require "include/dbms.inc.php";
-    require "include/template2.inc.php";
-    require "include/auth.inc.php";
+require "include/config.inc.php";
+require "include/dbms.inc.php";
+require "include/template2.inc.php";
+require "include/auth.inc.php";
 
-    $main = new Template("frame");
-    $body = new Template("employer_profile");
+$main = new Template("frame");
+$body = new Template("employer_profile");
 
-    $main->setContent("body", $body->get());
+$main->setContent("body", $body->get());
 
-    $main->close();
-
-?>
+$main->close();
