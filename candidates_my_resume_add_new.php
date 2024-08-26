1ea440447963f86e38cd7054c1c045ed59487b80
StefanoElisio <stfnelisio.work@gmail.com>
2024-08-26 19:03:26 +0200

    templating my resume add new

--- candidates_my_resume_add_new.php
+++ candidates_my_resume_add_new.php
@@ -1,19 +1,34 @@
 <?php
+session_start();
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-    require "include/config.inc.php";
-    require "include/dbms.inc.php";
-    require "include/template2.inc.php";
-    require "include/auth.inc.php";
+require "include/config.inc.php";
+require "include/dbms.inc.php";
+require "include/template2.inc.php";
+require "include/auth.inc.php";
 
-    $main = new Template("frame");
-    $body = new Template("candidates_my_resume_add_new");
+$main = new Template("frame");
+$body = new Template("candidates_my_resume_add_new");
 
-    $main->setContent("body", $body->get());
+$profile_id = $mysqli->query("SELECT profile.id FROM `profile` JOIN `user` ON user.id = profile.user_id WHERE user.username = '{$_SESSION["user"]["username"]}'");
+$profile_id = ($profile_id->fetch_assoc()['id']);
 
-    $main->close();
+$result = $mysqli->query("
+    SELECT
+        candidate.name AS name,
+        candidate.surname AS surname
+    FROM candidate
+    ");
+
+$data = $result->fetch_assoc();
+$body->setContent("name", $data['name']);
+$body->setContent("surname", $data['surname']);
+
+$main->setContent("body", $body->get());
+
+$main->close();
 
 ?>
