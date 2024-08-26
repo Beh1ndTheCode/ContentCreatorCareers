aad822b79ff8c7253697a2084848f7e8337a7254
StefanoElisio <stfnelisio.work@gmail.com>
2024-08-26 17:12:34 +0200

    candidate single templating

--- candidates_single.php
+++ candidates_single.php
@@ -9,16 +9,15 @@ require "include/config.inc.php";
 require "include/dbms.inc.php";
 require "include/template2.inc.php";
 require "include/auth.inc.php";
-require "get_by_id.inc.php";
+require "include/get_by_id.inc.php";
 
 $main = new Template("frame");
 $body = new Template("candidates_single");
 
-//verifica presenza immagine profilo
 $profile_id = $mysqli->query("SELECT profile.id FROM `profile` JOIN `user` ON user.id = profile.user_id WHERE user.username = '{$_SESSION["user"]["username"]}'");
 $profile_id = ($profile_id->fetch_assoc()['id']);
 
-$img = get_img($mysqli,$profile_id);
+$img = get_img($mysqli, $profile_id);
 $body->setContent("image", $img);
 
 $result = $mysqli->query("
@@ -26,6 +25,7 @@ $result = $mysqli->query("
         candidate.name AS name,
         candidate.surname AS surname,
         candidate.age AS age,
+        candidate.about AS about,
         expertise.title AS job_title,
         profile.phone AS phone_num,
         profile.email AS email,
@@ -38,23 +38,58 @@ $result = $mysqli->query("
     JOIN address ON address.profile_id = profile.id
     WHERE profile.id = '$profile_id'
 ");
-
 $data = $result->fetch_assoc();
 $body->setContent("name", $data['name']);
 $body->setContent("surname", $data['surname']);
 $body->setContent("age", $data['age']);
+$body->setContent("about", $data['about']);
 $body->setContent("job_title", $data['job_title']);
 $body->setContent("phone_num", $data['phone_num']);
 $body->setContent("email", $data['email']);
 $body->setContent("country", $data['country']);
 $body->setContent("city", $data['city']);
 
-$skills = get_skills($mysqli,$profile_id);
-$skills_html = '';
+$skills = get_skills($mysqli, $profile_id);
+$top_skills_html = '';
+$detail_skills_html = '';
 foreach ($skills as $skill) {
-    $skills_html .= "<span>{$skill}</span>";
+    $top_skills_html .= "<span>{$skill['name']}</span>";
+    $detail_skills_html .= "<div class='progress-sec style2'>
+								<span>{$skill['name']}</span>
+								<div class='progressbar'>
+									<i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i><i></i>
+								    <div class='progress'>";
+	for($i=1;$i<=$skill['level'];$i++)
+        $detail_skills_html .= "<i></i>";
+    $detail_skills_html .= "</div></div><p>{$skill['level']}0%</p></div>";
 }
-$body->setContent("skills", $skills_html);
+$body->setContent("top_skills", $top_skills_html);
+$body->setContent("detail_skills", $detail_skills_html);
+
+$jobs = get_jobs($mysqli, $profile_id);
+$jobs_html = '';
+foreach ($jobs as $job) {
+    $jobs_html .= " <div class='edu-history style2'>
+		                <i></i>
+		                <div class='edu-hisinfo'>
+		                    <h3>{$job['name']} <span>{$job['emp_name']}</span></h3>
+		                    <i>{$job['start']}  to  {$job['end']}</i>
+	                        <p>{$job['description']}</p>
+		                </div>
+	                </div>";
+}
+$body->setContent("jobs", $jobs_html);
+
+$portfolio = get_portfolio($mysqli, $profile_id);
+$portfolio_html = '';
+foreach ($portfolio as $img) {
+    $portfolio_html .= "<div class='mp-col'>
+							<div class='mportolio'><img src='{$img}'
+								alt='' /><a href='#' title=''><i class='la la-search'></i></a>
+							</div>
+						</div>";
+}
+$body->setContent("portfolio", $portfolio_html);
 
 $main->setContent("body", $body->get());
 $main->close();
--- include/get_by_id.inc.php
+++ include/get_by_id.inc.php
@@ -1,7 +1,7 @@
 <?php
 function get_img($mysqli, $profile_id)
 {
-    $img = $mysqli->query("SELECT image.path FROM `image` WHERE image.profile_id = '$profile_id'");
+    $img = $mysqli->query("SELECT image.path FROM `image` WHERE image.profile_id = '$profile_id' and image.type = 'profilo'");
     //$img = a immagine profilo se esiste, altrimenti è impostata su un placeholder
     if ($img->num_rows == 0)
         return "skins/jobhunt/images/profile.png";
@@ -10,9 +10,9 @@ function get_img($mysqli, $profile_id)
 
 function get_skills($mysqli, $profile_id)
 {
-    $sql_skills = $mysqli->query("SELECT skill.name FROM `skill` WHERE skill.candidate_id = '$profile_id'");
+    $sql_skills = $mysqli->query("SELECT skill.name,skill.level FROM `skill` WHERE skill.candidate_id = '$profile_id'");
     while ($row = $sql_skills->fetch_assoc())
-        $skills[] = $row['name']; // Aggiungi ogni skill all'array
+        $skills[] = ['name' => $row['name'], 'level' => $row['level']]; // Aggiungi ogni skill all'array
     return $skills;
 }
 
@@ -22,5 +22,41 @@ function get_socials($mysqli, $profile_id)
     while ($row = $sql_social->fetch_assoc())
         $socials[] = ['name' => $row['name'], 'uri' => $row['uri']];
     return $socials;
+}
+function get_jobs($mysqli, $profile_id)
+{
+    $sql_job = $mysqli->query("
+    SELECT
+        job.name AS name,
+        job.first_work_date AS start,
+        job.last_work_date AS end,
+        job.description AS description,
+        employer.name AS emp_name
+    FROM `job`
+        JOIN `profile` ON profile.id = job.candidate_id 
+        JOIN `employer` ON employer.id = job.employer_id 
+        WHERE profile.id = '$profile_id'
+    ");
+    while ($row = $sql_job->fetch_assoc()) {
+        if ($row['end'] == null)
+            $row['end'] = 'now';
+        $jobs[] = [
+            'name' => $row['name'],
+            'emp_name' => $row['emp_name'],
+            'start' => $row['start'],
+            'end' => $row['end'],
+            'description' => $row['description']
+        ];
+    }
+    return $jobs;
+}
+
+function get_portfolio($mysqli, $profile_id)
+{
+    $sql_imgs = $mysqli->query("SELECT image.path FROM `image` WHERE image.profile_id = '$profile_id' and image.type = 'portfolio'");
+    while ($row = $sql_imgs->fetch_assoc()) {
+        $imgs[] = $row['path'];
+    }
+    return $imgs;
 }
 ?>
