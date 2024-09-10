4825be9610545740c264b2327f0d385134cc83a1
Marco Ciucci <marco.ciucci@proton.me>
2024-09-10 17:41:20 +0200

    small fix

--- content_menagment.php
+++ content_menagment.php
@@ -11,27 +11,19 @@ $main = new Template("frame");
 $body = new Template("content_menagment");
 
 $result = $mysqli->query("
-    SELECT 
-        service_id,service.name
+    SELECT service_id, service.name
     FROM content
     JOIN service ON service.id = content.service_id
 ");
-$pages = [];
-while ($row = $result->fetch_assoc()){
-    $pages[] = [
-        'id' => $row['service_id'],
-        'name' => $row['name']
-    ];
-}
+
 $pages_html = '';
-foreach ($pages as $page) {
-    $id = $page['id'];
-    $name = $page['name'];
+while ($row = $result->fetch_assoc()) {
+    $id = $row['service_id'];
+    $name = $row['name'];
     $pages_html .= "<option value='$id'>$name</option>";
 }
+
 $body->setContent("pages", $pages_html);
 
 $main->setContent("body", $body->get());
 $main->close();
-
-?>
